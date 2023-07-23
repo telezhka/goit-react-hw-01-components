da9515abc0826f0cb96a1882dcd092bda78311cb
Roman Telik <rtelik58@gmail.com>
2023-07-23 21:59:35 +0300

    style-transact

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,19 +1,20 @@
+import css from '../styles/Transactions.module.css'
 export const TransactionHistory = ({ items }) => (
-    <table className="transaction-history">
+    <table className={css.transactionHistory}>
   <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+    <tr className={css.tr}>
+      <th className={css.th}>Type</th>
+      <th className={css.th}>Amount</th>
+      <th className={css.th}>Currency</th>
     </tr>
   </thead>
 
         <tbody>
             {items.map(({ id, type, amount, currency }) => 
-                <tr key={id}>
-                    <td>{type}</td>
-                    <td>{amount}</td>
-                    <td>{currency}</td>
+                <tr key={id} className={css.tr}>
+                    <td className={css.td}>{type}</td>
+                    <td className={css.td}>{amount}</td>
+                    <td className={css.td}>{currency}</td>
                 </tr>
             )}
         </tbody>
