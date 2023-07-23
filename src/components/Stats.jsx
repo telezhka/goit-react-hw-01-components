1c72b8c1e17cd41c87c49127083ddca1aae3c5b4
Roman Telik <rtelik58@gmail.com>
2023-07-23 14:58:37 +0300

    stays-done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,13 +1,12 @@
-export const Statistics = ({ title, stats }) => {
+export const Statistics = ({ title, stats }) => (
  <section className="statistics">
   <h2 className="title">{title}</h2>
-
      <ul className="stat-list">
-         {stats.map((id, label, percentage) => 
-         <li className="item" key={id}>
+            {stats.map(({ id, label, percentage }) => 
+            <li className="item" key={id}>
                  <span className="label">{label}</span>
-                 <span className="percentage">{percentage}</span>
-    </li>)}
+                 <span className="percentage">  {percentage}%</span>
+            </li>)}
   </ul>
 </section>
-}
+)
